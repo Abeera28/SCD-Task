7e06a0800208004b88c4d936fb7e836664ef7d48
Abeera <abeeranadeem977@gmail.com>
2026-03-10 20:46:13 +0500

    Updated Java lab code

--- lab1.java
+++ lab1.java
@@ -1,4 +1,4 @@
 public class lab1
 { public static void main(String[] args) 
-    { System.out.println("SCD Lab task of Abeera Nadeem"); } 
+    { System.out.println("Updated Version !!! SCD Lab task of Abeera Nadeem "); } 
 }
