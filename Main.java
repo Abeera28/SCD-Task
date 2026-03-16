f653f8bc8702d8f4d3ff1a8b8fe22674a896013d
Abeera <abeeranadeem977@gmail.com>
2026-03-16 09:27:27 +0500

    Added Students class and Main class for SCD Lab task

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Students s1 = new Students();
+        s1.name = "Abeera";
+        s1.rollNumber = 5040;
+
+
+        Students s2 = new Students();
+        s2.name = "Nadeem";
+        s2.rollNumber = 1234;
+
+
+        s1.displayInfo();
+        s2.displayInfo();
+
+
+    }
+    
+}
---
+++ Students.java
@@ -0,0 +1,14 @@
+//encapuslation attributes and function same
+public class Students{
+    String name;
+    int rollNumber;
+
+    public void displayInfo(){
+        System.out.println("Student name is :" + name);
+        System.out.println("Student roll number is :" + rollNumber);
+
+
+        System.out.println("------------------------------");
+
+    }
+}
